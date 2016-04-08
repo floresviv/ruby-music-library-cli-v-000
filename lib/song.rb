4ec86b41f6a219a3415815e9b25b91cb42d79e63
github username <github email address>
2016-04-08 13:55:08 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
 	
 	@@all = []
 
-	def initialize(name, artist = " ",  genre = " ")
+	def initialize(name, artist = nil,  genre = " ")
 		@name = name
 		@artist = artist
 		@genre = genre
